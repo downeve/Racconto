685a379f2b44a254d071356ad9de82ea439536a2
downeve <downeve@gmail.com>
2026-04-17 19:06:39 +0200

    fix: 로컬 개발 환경 실행 명령어 수정

--- dev.sh
+++ dev.sh
@@ -15,7 +15,7 @@ npm run dev &
 FRONTEND_PID=$! # 프론트엔드 프로세스 ID 저장
 
 # 🛑 꿀팁: Ctrl+C (종료) 누를 때 백엔드/프론트엔드 프로세스 한 번에 죽이기 (포트 꼬임 방지)
-trap "echo '🛑 개발 서버를 모두 종료합니다...'; kill $BACKEND_PID $FRONTEND_PID; exit" SIGINT
+trap "echo '🛑 개발 서버를 모두 종료합니다...'; kill $BACKEND_PID $FRONTEND_PID; exit" SIGINT SIGTERM EXIT
 
 # 백그라운드 작업들이 끝날 때까지 대기 (이게 있어야 터미널이 안 꺼집니다)
 wait
