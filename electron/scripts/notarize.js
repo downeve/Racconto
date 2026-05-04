d4767ee3a0ad11dd5ed90ddfaab41128228c8483
downeve <downeve@gmail.com>
2026-05-04 16:26:17 +0200

    fix: 깃허브 맥빌드 한계 20분 추가 / 애플 앱 디벨로퍼 서버 추가 동의 및 인증 진행

--- electron/scripts/notarize.js
+++ electron/scripts/notarize.js
@@ -1,16 +1,18 @@
-const { notarize } = require('@electron/notarize')
+const { notarize } = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context
-  if (electronPlatformName !== 'darwin') return
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') return;
 
-  const appName = context.packager.appInfo.productFilename
+  const appName = context.packager.appInfo.productFilename;
+
+  console.log(`  • 공증 시작: ${appName}`); // 로그를 추가하여 어디서 멈추는지 확인
 
   return await notarize({
     tool: 'notarytool',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD, // 이 환경변수 이름 확인!
     teamId: process.env.APPLE_TEAM_ID,
-  })
-}
+  });
+};
