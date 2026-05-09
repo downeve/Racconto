8d6d315cd162c14cd4e1a95420c4a0ddffe758a8
downeve <downeve@gmail.com>
2026-05-09 08:27:31 +0200

    fix: i18n index.ts ja.json 등록

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -4,6 +4,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from './locales/en.json';
 import ko from './locales/ko.json';
+import ja from './locales/ja.json';
 
 i18n
   .use(LanguageDetector) // 브라우저 언어 감지
@@ -12,6 +13,7 @@ i18n
     resources: {
       en: { translation: en },
       ko: { translation: ko },
+      ja: { translation: ja }
     },
     fallbackLng: 'en', // 기본 언어를 영어로 설정
     nonExplicitSupportedLngs: true,
